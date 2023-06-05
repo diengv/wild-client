6e015a59d25c6b3b77c03644cf41dff2a3dd0954
hungnm18 <hungnm18@bidv.com.vn>
2023-06-05 17:01:54 +0700

    code

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -5,16 +5,19 @@
         <NuxtLink>
           <img src="~/assets/images/wild-logo.svg">
         </NuxtLink>
-        <div class="s__header--arrow" v-if="!showSearch">
+        <div class="s__header--arrow" :class="{'active': activeNav}" @click="showBoxNav()">
           <img src="~/assets/images/icon-arrow-menu.svg">
         </div>
       </div>
       <div class="s__header--search" :class="{'s__header--search__active': showSearch}" @click="hasBoxSearch()">
-        <span v-if="!showSearch">Tìm kiếm trải nghiệm...</span>
-        <span v-if="!showSearch"><img src="~/assets/images/icon-search.svg"></span>
-        <div v-if="showSearch" class="s__header--search__text">
+        <span v-if="!showSearch && !activeNav">Tìm kiếm trải nghiệm...</span>
+        <span v-if="!showSearch && !activeNav"><img src="~/assets/images/icon-search.svg"></span>
+        <div v-if="showSearch && !activeNav" class="s__header--search__text">
           <span>TÌM KIẾM</span>
           <span>TRẢI NGHIỆM</span>
+        </div>
+        <div v-if="activeNav" class="s__header--search__text">
+          <span>“Adventure is not only a trip, but also a rush of adrenaline.”</span>
         </div>
       </div>
       <div class="s__header--lang">
@@ -23,6 +26,9 @@
         </div>
         <div v-if="!showSearch" class="s__header--lang__right">
           +84 886 677 950
+        </div>
+        <div v-if="activeNav" class="s__header--lang__right">
+          <img class="icon-close" @click="closeNav()" src="~/assets/images/icon-close.svg">
         </div>
       </div>
       <div v-if="showSearch" class="s__header--box-search">
@@ -55,8 +61,8 @@
           </div>
           <div class="s__header--box-search__wrap--book">
             <div class="s__header--box-search__wrap--label">
-             <span> Loại đặt chỗ</span>
-             <span><img src="~/assets/images/icon-help.svg"></span>
+              <span> Loại đặt chỗ</span>
+              <span><img src="~/assets/images/icon-help.svg"></span>
             </div>
             <div @click="showBoxBook()" class="s__header--box-search__wrap--input" :class="{'active' : locationActive}">
               <span v-if="!bookChoosed">Chọn loại đặt chỗ</span>
@@ -164,6 +170,128 @@
           </div>
         </div>
       </div>
+      <div class="s__header--box-nav" v-if="activeNav">
+        <div class="s__header--box-nav__left">
+          <div v-if="itemsNav.length > 0" v-for="item in itemsNav" class="item" :class="{'active': item.active}"
+               @click="activeItemNav(item)">
+            {{ item.name }}
+          </div>
+        </div>
+        <div class="s__header--box-nav__right">
+          <div v-if="rightNavLocation" class="s__header--box-nav__right--location">
+            <div class="s__header--box-nav__right--location-wrap-items" v-if="!rightNavLocationItemDetail">
+              <div @click="showDetailLocation()" class="s__header--box-nav__right--location-wrap-items__item">
+                <div class="img-item">
+                  <img src="~/assets/images/vn.png">
+                </div>
+                <div class="title-item">
+                  Việt Nam
+                </div>
+              </div>
+              <div class="s__header--box-nav__right--location-wrap-items__item">
+                <div class="img-item">
+                  <img src="~/assets/images/vn.png">
+                </div>
+                <div class="title-item">
+                  Việt Nam
+                </div>
+              </div>
+            </div>
+            <div class="s__header--box-nav__right--location-wrap-item-detail" v-if="rightNavLocationItemDetail">
+              <div class="left">
+                <div class="left__item">
+                  Nha Trang
+                </div>
+                <div class="left__item">
+                  Hà Giang
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+                <div class="left__item">
+                  Cao Bằng
+                </div>
+
+              </div>
+              <div class="right">
+                <div class="right__name">
+                  Việt Nam
+                </div>
+                <div class="right__image">
+                  <img src="~/assets/images/location-vn.jpg">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div v-if="rightNavLevel" class="s__header--box-nav__right--level">
+            <div class="s__header--box-nav__right--level__item">
+              <div class="img-item">
+                <img src="~/assets/images/lv.png">
+              </div>
+              <div class="title-item">
+                Người mới
+              </div>
+            </div>
+            <div class="s__header--box-nav__right--level__item">
+              <div class="img-item">
+                <img src="~/assets/images/lv.png">
+              </div>
+              <div class="title-item">
+                Người mới
+              </div>
+            </div>
+            <div class="s__header--box-nav__right--level__item">
+              <div class="img-item">
+                <img src="~/assets/images/lv.png">
+              </div>
+              <div class="title-item">
+                Người mới
+              </div>
+            </div>
+            <div class="s__header--box-nav__right--level__item">
+              <div class="img-item">
+                <img src="~/assets/images/lv.png">
+              </div>
+              <div class="title-item">
+                Người mới
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -180,7 +308,62 @@ export default {
     activeBook: false,
     locationChoosed: '',
     activityChoosed: '',
-    bookChoosed: ''
+    bookChoosed: '',
+    activeNav: false,
+    rightNavLocation: false,
+    rightNavLevel: false,
+    rightNavLocationItemDetail: false,
+    itemsNav: [
+      {
+        id: 1,
+        name: 'Trang chủ',
+        active: true,
+        hasHref: false,
+        href: '#',
+      },
+      {
+        id: 2,
+        name: 'Địa điểm',
+        active: false,
+        hasHref: false,
+        href: '#',
+      },
+      {
+        id: 3,
+        name: 'Cấp độ',
+        active: false,
+        hasHref: false,
+        href: '#',
+      },
+      {
+        id: 4,
+        name: 'Loại hoạt động',
+        active: false,
+        hasHref: false,
+        href: '#',
+      },
+      {
+        id: 5,
+        name: 'Bộ sưu tập',
+        active: false,
+        hasHref: false,
+        href: '#',
+      },
+      {
+        id: 6,
+        name: 'Mã giảm giá',
+        active: false,
+        hasHref: false,
+        href: '#',
+      },
+      {
+        id: 7,
+        name: 'intoWild Life',
+        active: false,
+        hasHref: false,
+        href: '#',
+      }
+    ]
   }),
   methods: {
     hasBoxSearch() {
@@ -191,6 +374,8 @@ export default {
     },
     showBoxLocation() {
       this.locationBox = !this.locationBox
+      this.activeBox = false
+      this.activeBook = false
     },
     locationChoose(local) {
       this.locationChoosed = local
@@ -198,9 +383,13 @@ export default {
     },
     showBoxActivity() {
       this.activeBox = !this.activeBox
+      this.locationBox = false
+      this.activeBook = false
     },
     showBoxBook() {
       this.activeBook = !this.activeBook
+      this.activeBox = false
+      this.locationBox = false
     },
     activeChoose(activity) {
       this.activeBox = false
@@ -209,6 +398,35 @@ export default {
     bookChoose(book) {
       this.activeBook = false
       this.bookChoosed = book
+    },
+    showBoxNav() {
+      this.activeNav = !this.activeNav
+      this.showSearch = true
+    },
+    closeNav() {
+      this.activeNav = false
+      this.showSearch = false
+    },
+    activeItemNav(item) {
+      this.itemsNav.forEach((val, i) => {
+        if (item.id === val.id) {
+          this.itemsNav[i].active = true
+          if (val.id === 2) {
+            this.rightNavLocation = true
+            this.rightNavLevel = false
+          }
+          else if(val.id === 3)
+          {
+            this.rightNavLevel = true
+            this.rightNavLocation = false
+          }
+        } else {
+          this.itemsNav[i].active = false
+        }
+      })
+    },
+    showDetailLocation() {
+      this.rightNavLocationItemDetail = true
     }
   }
 }
