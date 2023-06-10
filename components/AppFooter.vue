f43ae32891e83d5b7af6c5141ee4bc228e83a72b
tuandn <nhutuan.do@10kn.io>
2023-06-10 22:51:20 +0700

    code

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,30 @@
 <template>
 <div>
-  test
+  <section class="s__footer">
+    <div class="s__footer register-info">
+        <div class="s__footer register-info__description">
+          Bạn đã sẵn sàng cập nhật những <br> hoạt động mới nhất cùng intoWild?
+        </div>
+        <div class="s__footer register-info__button">
+          <a href="#">
+            Đăng ký nhận thông tin
+          </a>
+        </div>
+    </div>
+    <div class="s__footer main-footer">
+      <div class="s__container">
+        <div class="s__regulations">
+          <div class="s__regulations--title">
+            Các quy định cần lưu ý:
+          </div>
+          <div class="s__regulations--description">
+            1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur tristique felis, eget consectetur enim facilisis id. Donec ut dolor a diam fringilla scelerisque. Curabitur eu bibendum felis. Suspendisse vel augue pretium, scelerisque lectus ut, tristique nisi. Nullam nunc nulla, vestibulum vitae interdum eu, porta id ligula. Mauris cursus blandit nulla, venenatis lobortis velit. <span><a href="#">Xem thêm</a> </span>
+          </div>
+        </div>
+        <div class="s__line"></div>
+      </div>
+    </div>
+  </section>
 </div>
 </template>
 
