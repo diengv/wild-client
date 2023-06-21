5e371b32d4727741c0f6b9ea26616621b5436014
ADMIN <hungnm.dev94@gmail.com>
2023-06-21 16:56:21 +0700

    code

--- pages/filter/index.vue
+++ pages/filter/index.vue
@@ -2,9 +2,21 @@
  <div class="s__current_page">
    <div class="s__container">
      <section class="s__head_filter">
-        <div class="s__head_filter-item">
-          <span></span>
+        <div class="s__head_filter--item">
+          <span>Địa điểm</span>
         </div>
+       <div class="s__head_filter--item">
+         <span>Cấp độ hoạt động</span>
+       </div>
+       <div class="s__head_filter--item">
+         <span>Ngày khởi hành</span>
+       </div>
+       <div class="s__head_filter--item">
+         <span>Loại đặt chỗ</span>
+       </div>
+       <div class="s__head_filter--item add-filter">
+        <span><img src="~/assets/images/filter.svg"></span> <span>Thêm bộ lọc: </span><span>1</span>
+       </div>
      </section>
    </div>
  </div>
