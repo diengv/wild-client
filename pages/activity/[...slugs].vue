c75c4a9b9b0cc3db37714be2ecaab120e513f53c
ADMIN <hungnm.dev94@gmail.com>
2023-06-28 04:48:47 +0700

    code

--- pages/activity/[...slugs].vue
+++ pages/activity/[...slugs].vue
@@ -23,7 +23,247 @@
         <div class="star__line"></div>
         <div class="star__share">
           <div class="star__share--icon"><img src="~/assets/images/icon-share.svg"></div>
-          <div class="star__share--text"><a href="#">Chia sẻ</a> </div>
+          <div class="star__share--text"><a href="#">Chia sẻ</a></div>
+        </div>
+      </div>
+      <div class="s__activity--level">
+        <span class="s__activity--level__title">
+          Cấp độ trải nghiệm:
+        </span>
+        <span class="s__activity--level__it">Người mới</span>
+        <span class="s__activity--level__help"><img src="~/assets/images/icon-helps.svg"></span>
+      </div>
+      <div class="s__activity--view__all">
+        <a href="#">Xem tất cả hình ảnh</a>
+      </div>
+      <div class="s__activity--view__images">
+        <div class="s__activity--view__images--left">
+          <div class="s__activity--view__images--item">
+            <img src="~/assets/images/im-1.png">
+          </div>
+        </div>
+        <div class="s__activity--view__images--right">
+          <div class="s__activity--view__images--item">
+            <img src="~/assets/images/im-2.png">
+          </div>
+          <div class="s__activity--view__images--item">
+            <img src="~/assets/images/im-3.png">
+          </div>
+        </div>
+      </div>
+      <div class="s__line"></div>
+      <div class="s__activity--info">
+        <div class="s__activity--info__left">
+          <div class="s__activity--info__left--short">
+            Kỹ thuật lặn RAID là một trong những kỹ thuật lặn mới nhất, kết hợp tất cả các ưu điểm của những bằng lặn
+            truyền thống. Chúng tôi, những chuyên gia duy nhất về kỹ thuật này ở VIệt Nam sẵn sàng chia sẻ kiến thức và
+            giúp bạn trở thành một thợ lặn chuyện nghiệp.
+          </div>
+          <div class="s__activity--info__left--intros">
+            <div class="s__activity--info__left--intros__item">
+              <div class="s__activity--info__left--intros__item--icon">
+                <img src="~/assets/images/icon-language.svg">
+              </div>
+              <div class="s__activity--info__left--intros__item--info">
+                <span class="i-label">Ngôn ngữ</span>
+                <span class="i-description">Tiếng Việt, Tiếng Anh, Tiếng Nga, Tiếng Hàn</span>
+              </div>
+            </div>
+            <div class="s__activity--info__left--intros__item">
+              <div class="s__activity--info__left--intros__item--icon">
+                <img src="~/assets/images/icon-time.svg">
+              </div>
+              <div class="s__activity--info__left--intros__item--info">
+                <span class="i-label">Thời gian</span>
+                <span class="i-description">3 ngày 2 đêm</span>
+              </div>
+            </div>
+            <div class="s__activity--info__left--intros__item">
+              <div class="s__activity--info__left--intros__item--icon">
+                <img src="~/assets/images/icon-person.svg">
+              </div>
+              <div class="s__activity--info__left--intros__item--info">
+                <span class="i-label">Số lượng</span>
+                <span class="i-description">14 người</span>
+              </div>
+            </div>
+            <div class="s__activity--info__left--intros__item">
+              <div class="s__activity--info__left--intros__item--icon">
+                <img src="~/assets/images/icon-reg-per.svg">
+              </div>
+              <div class="s__activity--info__left--intros__item--info">
+                <span class="i-label">Đăng ký tối thiểu</span>
+                <span class="i-description">01 người</span>
+              </div>
+            </div>
+            <div class="s__activity--info__left--intros__item">
+              <div class="s__activity--info__left--intros__item--icon">
+                <img src="~/assets/images/icon-weather.svg">
+              </div>
+              <div class="s__activity--info__left--intros__item--info">
+                <span class="i-label">Thời điểm tổ chức</span>
+                <span class="i-description">Tháng 9 đến tháng 12</span>
+              </div>
+            </div>
+            <div class="s__activity--info__left--intros__item">
+              <div class="s__activity--info__left--intros__item--icon">
+                <img src="~/assets/images/icon-warning.svg">
+              </div>
+              <div class="s__activity--info__left--intros__item--info">
+                <span class="i-label">Không phù hợp với</span>
+                <span class="i-description">Người có bệnh tim, cao huyết áp, bệnh về thần kinh, người nhẹ hơn 35kg và phụ nữ mang thai</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="s__activity--info__right">
+          <div class="s__activity--info__right--head">
+            <div class="s__activity--info__right--head__item">
+              <div class="s__activity--info__right--head__item--top">
+                <div class="s__activity--info__right--head__item--top__icon">
+                  <img src="~/assets/images/icon-security.svg">
+                </div>
+                <div class="s__activity--info__right--head__item--top__label">
+                  HOÀN HỦY
+                  LINH HOẠT
+                </div>
+              </div>
+              <div class="s__activity--info__right--head__item--content">
+                Bạn sẽ nhận lại 100% giá trị cọc nếu thông báo huỷ trước ngày khởi hành theo quy định
+              </div>
+            </div>
+            <div class="s__activity--info__right--head__item">
+              <div class="s__activity--info__right--head__item--top">
+                <div class="s__activity--info__right--head__item--top__icon">
+                  <img src="~/assets/images/icon-safety.svg">
+                </div>
+                <div class="s__activity--info__right--head__item--top__label">
+                  THANH TOÁN AN TOÀN
+                </div>
+              </div>
+              <div class="s__activity--info__right--head__item--content">
+                Hỗ trợ đổi ngày đăng ký và thông tin của bạn được bảo mật ở tất cả các phương thức thanh toán
+              </div>
+            </div>
+          </div>
+          <div class="s__activity--info__right--bottom">
+            <div class="s__activity--info__right--bottom__price-old">
+              4.600.000 VND/người
+            </div>
+            <div class="s__activity--info__right--bottom__price-sale">
+              <span>Giá từ</span>
+              <span>4.400.000 VND/người</span>
+            </div>
+            <div class="s__activity--info__right--bottom__description">Đăng ký trước, thanh toán sau</div>
+            <div class="s__activity--info__right--bottom__choose-date">
+              <button class="btn btn-choose-date">Chọn ngày</button>
+            </div>
+            <div class="s__line"></div>
+            <div class="s__activity--info__right--bottom__coupons">
+              <div class="s__activity--info__right--bottom__coupons--label">
+                2 mã giảm giá:
+              </div>
+              <div class="s__activity--info__right--bottom__coupons--items">
+                <div class="coupon-item">
+                  Giảm 10%
+                </div>
+                <div class="coupon-item">
+                  Giảm 200K
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="s__activity--box">
+        <div class="s__activity--box__title">
+          <h2>Trải nghiệm nổi bật</h2>
+        </div>
+        <div class="s__activity--box__content mb-104">
+          <Carousel :items-to-show="3">
+            <Slide v-for="slide in 10" :key="slide">
+              <div class="carousel__item">
+                <div class="s__activity--experience">
+                  <a href="#">
+                    <div class="s__activity--experience__icon">
+                      <img src="~/assets/images/icon-biking.svg">
+                    </div>
+                    <div class="s__activity--experience__description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </Slide>
+            <template #addons>
+              <Navigation/>
+            </template>
+          </Carousel>
+        </div>
+      </div>
+      <div class="s__activity--box">
+        <div class="s__activity--box__title">
+          <h2>Thông tin chuyến đi</h2>
+        </div>
+        <div class="s__activity--box__content mb-104">
+          <div class="s__activity--info-travel">
+            <div class="s__activity--info-travel__left">
+              <div class="s__activity--info-travel__left--description">
+                Đây là khóa học kéo dài 03 ngày, bước đầu tiên để bạn trở thành thợ lặn bình khí có chứng chỉ. Chứng chỉ
+                DiveRAID mở ra cơ hội để bạn tự do lặn ở khắp nơi trên Thế giới cũng như chủ động lặn một mình ở độ sâu
+                tối đa lên tới 20m. Xuyên suốt khóa huấn luyện, bạn sẽ được trang bị kiến thức nền tảng quan trọng về
+                bình dưỡng khí, các kiểm soát cơ thể và những kỹ năng thiết yếu ở dưới mặt nước. Khóa học sẽ bao gồm
+                buổi tập ở hồ bơi sâu và 04 lần lặn thực tế ở biển.
+              </div>
+              <div class="s__activity--info-travel__left--convetrate-place">
+                <div class="icon-convetrate-place">
+                  <img src="~/assets/images/icon-point.svg">
+                </div>
+                <div class="info-convetrate-place">
+                  <span>Địa điểm tập trung</span>
+                  <span>Khách sạn của bạn tại Nha Trang.</span>
+                </div>
+              </div>
+            </div>
+            <div class="s__activity--info-travel__right">
+              <img src="~/assets/images/im-map.png">
+            </div>
+          </div>
+          <div class="s__activity--dates">
+            <div @click="showContentDate(date)" v-for="date in listDates" class="s__activity--dates__item" :class="{'active' : date.active}">
+              <div class="s__activity--dates__item--title">
+                {{ date.title }}
+              </div>
+              <div v-if="date.active" class="s__activity--dates__item--content">
+                <div class="list-activity-note">
+                  <div class="list-activity-note__item">
+                    <span><img src="~/assets/images/icon-moon.svg"></span>
+                    <span class="strong-text">Ngủ đêm:</span>
+                    <span>Tự túc.</span>
+                  </div>
+                  <div class="list-activity-note__item">
+                    <span><img src="~/assets/images/icon-eat.svg"></span>
+                    <span class="strong-text">Bữa ăn:</span>
+                    <span> Không bao gồm.</span>
+                  </div>
+                  <div class="list-activity-note__item">
+                    <span><img src="~/assets/images/icon-activity.svg"></span>
+                    <span class="strong-text">Hoạt động chính: </span>
+                    <span>Diving</span>
+                  </div>
+                  <div class="list-activity-note__content">
+                    {{ date.content }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="s__activity--wild-buddy">
+            <div class="s__activity--wild-buddy__left">
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -31,8 +271,50 @@
 </template>
 
 <script>
+import {Carousel, Slide, Navigation} from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
 export default {
-  name: "index"
+  name: "index",
+  components: {
+    Carousel,
+    Slide,
+    Navigation
+  },
+  data: () => ({
+    listDates: [
+      {
+        id: 1,
+        active: true,
+        title: 'NGÀY 1 - HỌC NHỮNG KIẾN THỨC NỀN TẢNG',
+        content: 'Bạn sẽ được trang bị kiến thức nền tảng quan trọng về bình dưỡng khí, các kiểm soát cơ thể và những kỹ năng thiết yếu ở dưới mặt nước. Khóa học sẽ cung cấp đến bạn một tài khoản trực tuyết để bạn dễ dàng xem lại các nội dung lý thuyết của chương trình bất cứ khi nào bạn cần. Ngày đầu tiên này, bạn cũng sẽ được làm quen với các kỹ thuật lặn DiveRAID ở hồ bơi sâu tại trung tâm Nha Trang.'
+      },
+      {
+        id: 2,
+        active: false,
+        title: 'NGÀY 2 - OPEN WATER',
+        content: 'Bạn sẽ được trang bị kiến thức nền tảng quan trọng về bình dưỡng khí, các kiểm soát cơ thể và những kỹ năng thiết yếu ở dưới mặt nước. Khóa học sẽ cung cấp đến bạn một tài khoản trực tuyết để bạn dễ dàng xem lại các nội dung lý thuyết của chương trình bất cứ khi nào bạn cần. Ngày đầu tiên này, bạn cũng sẽ được làm quen với các kỹ thuật lặn DiveRAID ở hồ bơi sâu tại trung tâm Nha Trang.'
+      },
+      {
+        id: 3,
+        active: false,
+        title: 'NGÀY 3 - DIVERAID 20M',
+        content: 'Bạn sẽ được trang bị kiến thức nền tảng quan trọng về bình dưỡng khí, các kiểm soát cơ thể và những kỹ năng thiết yếu ở dưới mặt nước. Khóa học sẽ cung cấp đến bạn một tài khoản trực tuyết để bạn dễ dàng xem lại các nội dung lý thuyết của chương trình bất cứ khi nào bạn cần. Ngày đầu tiên này, bạn cũng sẽ được làm quen với các kỹ thuật lặn DiveRAID ở hồ bơi sâu tại trung tâm Nha Trang.'
+      }
+    ],
+  }),
+  methods: {
+    showContentDate(date) {
+      this.listDates.forEach((val) => {
+        if (val.id === date.id) {
+          val.active = true
+        } else {
+          val.active = false
+        }
+      })
+    }
+  }
 }
 </script>
 
