899758374ad91298540b53719f70ef7867aafdb5
hung.nguyen <hung.nguyenmanh@10kn.io>
2023-06-27 12:28:42 +0700

    code

---
+++ pages/activity/[...slugs].vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="s__activity">
+    <div class="s__container">
+      <div class="s__activity--breadcrumb">
+        <a class="s__activity--breadcrumb__item" href="#">Trang chủ</a>
+        <a class="s__activity--breadcrumb__item" href="#">Người mới</a>
+        <span
+            class="s__activity--breadcrumb__current">#DiveRAID - Khóa huấn luyện lặn sâu 3 ngày cùng Dive Master Shawn</span>
+      </div>
+      <div class="s__activity--title">
+        <h1>LeTu Adventure - Khóa huấn luyện lặn sâu 3 ngày cùng Dive Master Shawn</h1>
+      </div>
+      <div class="s__activity--star-share">
+        <div class="star">
+          <span><img src="~assets/images/star.svg"></span>
+          <span><img src="~assets/images/star.svg"></span>
+          <span><img src="~assets/images/star.svg"></span>
+          <span><img src="~assets/images/star.svg"></span>
+          <span><img src="~assets/images/star.svg"></span>
+        </div>
+        <div class="star__point">4.6</div>
+        <div class="star__rate"><a href="#">5 đánh giá</a></div>
+        <div class="star__line"></div>
+        <div class="star__share">
+          <div class="star__share--icon"><img src="~/assets/images/icon-share.svg"></div>
+          <div class="star__share--text"><a href="#">Chia sẻ</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
